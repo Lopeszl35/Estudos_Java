de43537e39d2491f59780b62195258216ec1bc48
Lopeszl35 <rafzl490@gmail.com>
2023-03-05 17:30:35 -0300

    Capitulo 3 classes objetos e metodos

--- estudoLivroJava/src/ControllerAccount/AccountTest.java
+++ estudoLivroJava/src/ControllerAccount/AccountTest.java
@@ -8,34 +8,30 @@ public class AccountTest {
 		//Cria um objeto Scanner para obter entrada a partir da janela de comando
 		Scanner input = new Scanner(System.in);
 		
-		//Cria um objeto Account e o atribui a myAccount
-		Account myAccount = new Account(); //Objeto myAccount não inicializado
-		Account myAccount1 = new Account("Rafael", 50.00);//Objeto inicializado com o valor "Rafael", e valor do saldo
-		Account myAccount2 = new Account("John Grene", -7.25);//Objeto inicializado com o valor "Rafael", e valor do saldo
-		Account nomeConta = new Account();//Objeto conta não inicializado
-		Account balance = new Account();//Objeto balance(saldo) não inicializado
+		//Declarando a Classe Account e seus objetos
+		Account account1 = new Account("Jane Green", 50.00);
+		Account account2 = new Account("John Blue", -7.53);
 		
-		System.out.printf("O saldo da conta de %s é %.2f\n", myAccount1.getName(), myAccount1.getBalance());
-		System.out.printf("O saldo da conta de %s é %.2f\n", myAccount2.getName(), myAccount2.getBalance());
+		//Ixibe saldo inicial de cada objeto
+		System.out.printf("%s balance: %.2f\n", account1.getName(), account1.getBalance());
+		System.out.printf("%s balance: %.2f\n", account2.getName(), account2.getBalance());
 		
-		//Solicita e le o nome
-		System.out.println("Please enter the name: ");
-		String theName = input.nextLine();// Lê a String informada pelo usuário
-		myAccount.setName(theName);//Insere o nome dado pelo usuário em theName, usando os métodos da clase Account
-		//Exibe o nome armazenado no objeto myAccount
-		System.out.printf("Name the Account is: %s\n", myAccount.getName());
+		System.out.print("Enter deposit amount for account1: ");
+		double depositAmount = input.nextDouble();//Obtem a entrada do usuário
+		System.out.printf("%nadding %.2f to account1 balance\n\n", depositAmount);
+		account1.deposit(depositAmount);//Adiciona o saldo a account1
 		
-		System.out.println("Please enter the name of the Account: ");
-		String nome = input.nextLine();
-		nomeConta.setName(nome);
+		//Exibe os saldos
+		System.out.printf("%s balance: $%.2f\n", account1.getName(), account1.getBalance());
+		System.out.printf("%s balance: $%.2f\n", account2.getName(), account2.getBalance());
 		
-		System.out.println("Please enter the balance of the Account: ");
-		double saldo = input.nextDouble();
-		balance.setBalance(saldo);
+		System.out.print("Enter deposit amount for account2: ");
+		depositAmount = input.nextDouble();
+		System.out.printf("%nadding %.2f to account2 balance\n\n", depositAmount);
+		account2.deposit(depositAmount);
 		
-		System.out.printf("O nome da conta informada é %s\n", nomeConta.getName());
-		System.out.printf("O saldo desta conta é %.2f", balance.getBalance());
-		
+		System.out.printf("%s balance: $%.2f\n", account1.getName(), account1.getBalance());
+		System.out.printf("%s balance: $%.2f\n", account2.getName(), account2.getBalance());
 		
 		
 		
