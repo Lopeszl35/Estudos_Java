3cedd36a04cab3561060331917a86ac18e5189ca
Lopeszl35 <rafzl490@gmail.com>
2023-03-05 17:57:53 -0300

    Gerando Caixas de dialágo

---
+++ estudoLivroJava/src/JanelaMensagemJava/DialagoCaixa.java
@@ -0,0 +1,20 @@
+package JanelaMensagemJava;
+import javax.swing.JOptionPane;
+
+public class DialagoCaixa {
+
+	public static void main(String[] args) {
+		
+		//Pede ao usuário para inserir seu nome
+		String nome =JOptionPane.showInputDialog("Qual o seu nome? ");
+		
+		//Cria a mensagem
+		String mensagem = String.format("Bem vindo %s, á programação Java", nome);
+		
+		
+		//Exibe uma caixa de mensagem
+		JOptionPane.showMessageDialog(null, mensagem);
+		
+	}
+
+}
