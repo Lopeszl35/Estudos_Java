811bb20f84851c66ebd7586fd8295d90b4e9983c
Lopeszl35 <rafzl490@gmail.com>
2023-03-11 11:34:52 -0300

    Atribuindo uma classe para calcular o raio e o volume, e instanciando um objeto na classe principal

--- curso_programacao/RaioVolume/RaioVolume/RaioVolume.java
+++ curso_programacao/RaioVolume/RaioVolume/RaioVolume.java
@@ -1,35 +1,28 @@
 package RaioVolume;
 import java.util.Scanner;
 
+import util.Calculator;
+
 public class RaioVolume {
-	//Atribuindo uma constante
-	public static final double PI = 3.14159;
 
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
+		Calculator calculator = new Calculator();
 		
 		System.out.print("Entre com o valor do raio: ");
 		double raio = input.nextDouble();
 		
-		double c = circunferencia(raio);
+		double c = calculator.circunferencia(raio);
 		
-		double v = volume(raio);
+		double v = calculator.volume(raio);
 		
 		System.out.printf("Cincunferência: %.2f%n", c);
 		System.out.printf("Volume: %.2f%n", v);
-		System.out.printf("Pi: %.2f%n", PI);
+		System.out.printf("Pi: %.2f%n", calculator.PI);
 		
 		
 		input.close();
 	}
 	
-	//Em Java os métodos na propria classe devem ser declarados com "static"
-	public static double circunferencia(double raio) {
-		return 2.0 * PI * raio;
-	}
-	
-	public static double volume(double raio) {
-		return 4.0 * PI * raio * raio * raio / 3.0;
-	}
 
 }
