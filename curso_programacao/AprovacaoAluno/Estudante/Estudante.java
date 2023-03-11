697d83ee2e40265bc36fc6b3be915b28746a2c5d
Lopeszl35 <rafzl490@gmail.com>
2023-03-11 17:25:03 -0300

    Utilizando Construtor inicializado

--- curso_programacao/AprovacaoAluno/Estudante/Estudante.java
+++ curso_programacao/AprovacaoAluno/Estudante/Estudante.java
@@ -3,7 +3,7 @@ package Estudante;
 public class Estudante {
 	private String[] nome;
 	private double[][] notas;
-	
+
 	public String[] getNome() {
 		return nome;
 	}
@@ -13,7 +13,7 @@ public class Estudante {
 	}
 
 	public double[][] getNotas() {
-	    return notas;
+		return notas;
 	}
 
 	public void setNotas(double[][] notas) {
@@ -21,28 +21,24 @@ public class Estudante {
 	}
 
 	public void alunoAprovacao(String[] nomes, double[][] notas) {
-	    for(int i = 0; i < nomes.length; i++) {
-	      double soma = notas[i][0] + notas[i][1] + notas[i][2];
-	      int pontos = 0;
-	        
-	      if(soma >= 60) {
-	          System.out.println("Aluno " + nomes[i] + " aprovado");
-	      }
-	      else {
-	        while(soma < 60) {
-		      pontos++;
-		      soma++;
-		    }
-	          System.out.println("Aluno " + nomes[i] + " reprovado");
-	          System.out.printf("Faltou + %d na nota para aprovação%n", pontos);
-	          System.out.println();
-	     }
-	     pontos = 0;
-	     soma = 0;
-	   }
+		for (int i = 0; i < nomes.length; i++) {
+			double soma = notas[i][0] + notas[i][1] + notas[i][2];
+			int pontos = 0;
+
+			if (soma >= 60) {
+				System.out.println("Aluno " + nomes[i] + " aprovado");
+			} else {
+				while (soma < 60) {
+					pontos++;
+					soma++;
+				}
+				System.out.println("Aluno " + nomes[i] + " reprovado");
+				System.out.printf("Faltou + %d na nota para aprovação%n", pontos);
+				System.out.println();
+			}
+			pontos = 0;
+			soma = 0;
+		}
 	}
 
-	
-	
-	
 }
--- curso_programacao/Dolar/cotacaoDolar/cotacaoDolar.java
+++ curso_programacao/Dolar/cotacaoDolar/cotacaoDolar.java
@@ -1,4 +1,5 @@
 package cotacaoDolar;
+
 import java.util.Scanner;
 
 import convertDolar.convertDolar;
@@ -7,15 +8,14 @@ public class cotacaoDolar {
 
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
-		
+
 		System.out.print("Informe a cotação do dolar: ");
 		double cotacao = input.nextDouble();
 		System.out.println("Quantos dolares você pretende comprar?: ");
 		double valor = input.nextDouble();
-		
-		System.out.println("Você ira pagar em reais o preço de: "+ convertDolar.dolar(cotacao, valor));
 
-		
+		System.out.println("Você ira pagar em reais o preço de: " + convertDolar.dolar(cotacao, valor));
+
 		input.close();
 	}
 
