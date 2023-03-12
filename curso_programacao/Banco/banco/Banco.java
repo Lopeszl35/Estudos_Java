feba6a43542f96f97f22d4abed5cf59d43dacd45
Lopeszl35 <rafzl490@gmail.com>
2023-03-11 21:39:57 -0300

    Banco conta deposito e saques

--- curso_programacao/Banco/banco/Banco.java
+++ curso_programacao/Banco/banco/Banco.java
@@ -23,7 +23,7 @@ public class Banco {
 		// Verifica se a entrada é válida
 		while (conditionStr.length() != 1 && !conditionStr.equalsIgnoreCase("N")
 				&& !conditionStr.equalsIgnoreCase("Y")) {
-			
+
 			System.out.println("Invalid input. Please enter a single character.");
 			conditionStr = input.nextLine();
 		}
@@ -32,78 +32,26 @@ public class Banco {
 			System.out.println("Enter initial deposit value: ");
 			balance = input.nextDouble();
 
-			//Instancia um objeto que recebera um deposito
-			Conta account = new Conta(numberAccount, name, balance);
+			// Instancia um objeto que recebera um deposito
+			Banco banco = new Banco();
+			Conta account = new Conta(numberAccount, name, balance, banco);
 
 			System.out.println("========= Account Data ===========");
 			System.out.println(account);
-			
-			System.out.println("what do you want to do next: ");
-			System.out.println("-0- Exit:");
-			System.out.println("-1- Deposit:");
-			System.out.println("-2- withdraw:");
-			int decision = input.nextInt();
-			
-			while(decision != 0 && decision != 1 && decision != 2) {
-				System.out.println("Invalid decision");
-				System.out.println("answer again: ");
-				decision = input.nextInt();
-			}
 
-			switch (decision) {
-				case 0:
-					System.exit(0);
-					break;
-				case 1:
-					System.out.println("Enter a deposit value: ");
-					balance = input.nextDouble();
-					account.depositoSaldo(balance);
-					System.out.println(account);
-					break;
-				case 2:
-					System.out.println("Enter a withdraw value: " );
-					balance = input.nextDouble();
-					account.saqueSaldo(balance);
-					System.out.println(account);
-					break;
-			}
+			// O usuário tomará uma decisão de depositar ou sacar dinheiro
+			account.decisions(account);
 		}
 
 		else if (conditionStr.equalsIgnoreCase("n")) {// Caso não tenha um depósito inicial
-			//Instancia um objeto não que recebera um deposito
-			Conta accountNotBalance = new Conta(numberAccount, name);
+			// Instancia um objeto não que recebera um deposito
+			Banco banco = new Banco();
+			Conta accountNotBalance = new Conta(numberAccount, name, banco);
 			System.out.println("========= Account Data ===========");
 			System.out.println(accountNotBalance);
-			
-			System.out.println("what do you want to do next: ");
-			System.out.println("-0- Exit:");
-			System.out.println("-1- Deposit:");
-			System.out.println("-2- withdraw:");
-			int decision = input.nextInt();
-			
-			while(decision != 0 && decision != 1 && decision != 2) {
-				System.out.println("Invalid decision");
-				System.out.println("answer again: ");
-				decision = input.nextInt();
-			}
 
-			switch (decision) {
-				case 0:
-					System.exit(0);
-					break;
-				case 1:
-					System.out.println("Enter a deposit value: ");
-					balance = input.nextDouble();
-					accountNotBalance.depositoSaldo(balance);
-					System.out.println(accountNotBalance);
-					break;
-				case 2:
-					System.out.println("Enter a withdraw value: " );
-					balance = input.nextDouble();
-					accountNotBalance.saqueSaldo(balance);
-					System.out.println(accountNotBalance);
-					break;
-			}
+			// O usuário tomará uma decisão de depositar ou sacar dinheiro
+			accountNotBalance.decisions(accountNotBalance);
 		}
 
 		input.close();
--- curso_programacao/Banco/conta/Conta.java
+++ curso_programacao/Banco/conta/Conta.java
@@ -1,19 +1,24 @@
 package conta;
 
-public class Conta {
+import java.util.Scanner;
+
+import banco.Banco;
+
+public class Conta extends Banco {
+
 	private int numeroConta;
 	private String nome;
 	private double saldoConta;
-	
-	
-	public Conta(int numeroConta, String nome, double saldoConta) {
+	private Banco banco;// Variavel para armazenar objeto Banco
+
+	public Conta(int numeroConta, String nome, double saldoConta, Banco banco) {
 		super();
 		this.numeroConta = numeroConta;
 		this.nome = nome;
 		this.saldoConta = saldoConta;
 	}
-	
-	public Conta(int numeroConta, String nome) {
+
+	public Conta(int numeroConta, String nome, Banco banco) {
 		super();
 		this.numeroConta = numeroConta;
 		this.nome = nome;
@@ -34,13 +39,53 @@ public class Conta {
 	public double getSaldoConta() {
 		return saldoConta;
 	}
-	
+
 	public void saqueSaldo(double saque) {
 		this.saldoConta -= saque;
 	}
-	
+
 	public void depositoSaldo(double deposito) {
 		this.saldoConta += deposito;
+	}
+
+	public void decisions(Banco account) {
+		Scanner input = new Scanner(System.in);
+		double balance;
+		int decision;
+
+		do {
+			System.out.println();
+			System.out.println("what do you want to do next: ");
+			System.out.println("-0- Exit:");
+			System.out.println("-1- Deposit:");
+			System.out.println("-2- withdraw:");
+			decision = input.nextInt();
+
+			while (decision != 0 && decision != 1 && decision != 2) {
+				System.out.println("Invalid decision");
+				System.out.println("answer again: ");
+				decision = input.nextInt();
+			}
+
+			switch (decision) {
+			case 0:
+				System.exit(0);
+				break;
+			case 1:
+				System.out.println("Enter a deposit value: ");
+				balance = input.nextDouble();
+				this.depositoSaldo(balance);
+				System.out.println(account);
+				break;
+			case 2:
+				System.out.println("Enter a withdraw value: ");
+				balance = input.nextDouble();
+				this.saqueSaldo(balance);
+				System.out.println(account);
+				break;
+			}
+		} while (decision != 0);
+		input.close();
 	}
 
 	@Override
