1f7f0789c626af5ddb4309b8d012d657bc8c1d1c
Lopeszl35 <rafzl490@gmail.com>
2023-06-11 21:36:28 -0300

    Usando lista para aumento de salário de funcionários

---
+++ curso_programacao/Employee/controlEmployee/ControlEmployee.java
@@ -0,0 +1,75 @@
+package controlEmployee;
+
+import java.util.List;
+
+public class ControlEmployee {
+	private Integer id;
+	private String name;
+	private Double salary;
+	
+	
+	public ControlEmployee() {
+	}
+
+
+	public ControlEmployee(Integer id, String name, Double salary) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.salary = salary;
+	}
+
+
+	public Integer getId() {
+		return id;
+	}
+
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	public Double getSalary() {
+		return salary;
+	}
+
+
+	public void setSalary(Double salary) {
+		this.salary = salary;
+	}
+	
+	public void increaseSalary(double percentage) {
+		salary += (salary * percentage / 100.0);
+	}
+	
+	public Integer positionID(List<ControlEmployee> list, int id) {
+		for(int i = 0; i < list.size(); i++) {
+			if(list.get(i).getId() == id) {
+				return i;
+			}
+		}
+		return null;
+	}
+
+
+	@Override
+	public String toString() {
+		return "ID = " + id + ", "
+				+ "Name = " + name + ", "
+				+ "Salary = " + salary;
+	}
+	
+	
+
+}
---
+++ curso_programacao/Employee/employee/Employee.java
@@ -0,0 +1,66 @@
+package employee;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
+
+import controlEmployee.ControlEmployee;
+
+public final class Employee {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner input = new Scanner(System.in);
+		
+		//Criando uma lista de funcionários
+		List<ControlEmployee> list = new ArrayList<>();
+		
+		System.out.println("How many employees will be registered: ");
+		int N = input.nextInt();
+		
+		for(int i = 0; i < N; i++) {
+			System.out.println(  );
+			System.out.println("Employee #"+(i+1) + ": ");
+			System.out.print("ID: ");
+			Integer id = input.nextInt();
+			System.out.println("Name: ");
+			input.nextLine();//Limpando o buffer do teclado
+			String name = input.nextLine();
+			System.out.println("Salary: ");
+			Double salary = input.nextDouble();
+			
+			ControlEmployee employee = new ControlEmployee(id, name, salary);
+			
+			
+			list.add(employee);
+		}
+		
+		System.out.println();
+		System.out.println("Enter the employee id that will have salary increase: ");
+		int idsalary = input.nextInt();
+		ControlEmployee position = new ControlEmployee();
+		Integer pos = position.positionID(list, idsalary);
+		
+		if(pos == null) {
+			System.out.print("This ID dois not exist");
+		}
+		else{
+			System.out.println("Enter the percentage: ");
+			double percent = input.nextDouble();
+			list.get(pos).increaseSalary(percent);
+		}
+		
+		System.out.println();
+		System.out.println("list of employees: ");
+		for (ControlEmployee employee: list) {
+			System.out.println(employee);
+		}
+		
+		
+		
+		
+		input.close();
+	}
+
+}
